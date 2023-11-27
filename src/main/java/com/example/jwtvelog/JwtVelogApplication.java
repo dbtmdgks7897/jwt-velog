f92e144becbd56c4bc1b859174ca3a410de67c4b
ysh <dbtmdgks7897@outlook.kr>
2023-11-27 10:19:25 +0900

    first

---
+++ src/main/java/com/example/jwtvelog/JwtVelogApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtvelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtVelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtVelogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwtvelog/JwtVelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtvelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtVelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
